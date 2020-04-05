6a30f27bcc0244b6d069a428e27163086afecf56
dpoveda <57077319+dpoveda@users.noreply.github.com>
2020-04-05 09:50:14 -0300

    Add files via upload

---
+++ ResoluciónDeExamenes/Examen00/Parte 3 - Ej10.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void cargaMasiva(int[], int&, int);
+float promedio (int[], int);
+
+int main(){
+	int n; 
+	int len = 0; 
+	
+	do{
+		cout<<"Ingrese un valor >=1"<<endl;
+		cin>>n; //cantidad de posiciones del vector definida por el usuario
+	}while(n<1);
+	
+	int a[n];
+	
+	cargaMasiva (a, len, n);
+	cout<<"Prom: "<<promedio(a, len)<<endl;
+
+	return 0;
+}
+
+void cargaMasiva(int v[], int &L, int cp){
+	for(int i=0; i<cp; i++){
+		cout<<"Ingrese un valor"<<endl;
+		cin>>v[i];
+		L++;
+	}
+	
+}
+
+float promedio (int v[], int L){
+	float sum = 0;
+	
+	for(int i=0; i<L; i++){
+		sum = sum + v[i];
+	}
+	
+	return sum/L;	
+}
+
+
